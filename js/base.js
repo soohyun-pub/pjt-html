a33e27217f82e308dd7a3c74ad08e46d04df56d2
soohyun <applemango7924@naver.com>
2018-12-18 21:18:11 +0900

    fst upload

---
+++ js/base.js
@@ -0,0 +1,20 @@
+/**************************************************************
+	Author : Kim Soo Hyun (Publisher)
+	Date : 2018-00-00
+	Version : 1.0
+**************************************************************/
+// jQuery.noConflict();
+// (function($){
+// 	   jquery code
+// })(jQuery);
+
+$(function(){
+	$('.header').slide();
+});
+
+
+$.fn.slide = function(){
+	return this.each(function(){
+		//alert('start');
+	});			
+};
